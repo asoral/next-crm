c53e5541a1e0161f016c906fe2657a3c00f7e752
Suman Munde <smunde@dexciss.com>
2025-08-18 10:46:39 +0530

    feat: removed user permissions

--- next_crm/api/views.py
+++ next_crm/api/views.py
@@ -5,11 +5,9 @@ from pypika import Criterion
 @frappe.whitelist()
 def get_views(doctype):
     View = frappe.qb.DocType("CRM View Settings")
-    query = (
-        frappe.qb.from_(View)
-        .select("*")
-        .where(Criterion.any([View.user == "", View.user == frappe.session.user]))
-    )
+    
+    query = frappe.qb.from_(View).select("*")
+
     if doctype:
         query = query.where(View.dt == doctype)
     views = query.run(as_dict=True)
